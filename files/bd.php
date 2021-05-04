62a9cd7ef94f01557ecee1b712404ff5a9049f4f
Sandrine <sandrine.hanna@etu.univ-lyon1.fr>
2021-05-04 12:27:08 +0200

    ajout de la connexion

--- files/bd.php
+++ files/bd.php
@@ -1,10 +1,57 @@
 <?php
 
-    $dbHost = "localhost";
-    $dbUser = "p1906670";
-    $dbPwd = "Prison27Suffix";
-    $dbName = "p1906670";
 
+
+    
+    if(isset($_Post['utilisateur'])){
+        $dbUser = $_POST['utilisateur'];
+        $dbPwd = $_POST['password'];
+        session_start();
+
+        $dbHost = "localhost";
+        $dbUser = "p1906670";
+        $dbPwd = "Prison27Suffix";
+        $dbName = "p1906670";
+
+        mysqli_connect($dbHost, $dbUser, $dbPwd);
+        mysqli_select_db($dbName);
+       
+        $dbUser = ereg_replace("[^A-Za-z0-9]", $dbUser);
+        $dbPwd = ereg_replace("[^A-Za-z0-9]", $dbPwd);
+
+        $dbPwd = shal($dbPwd);
+
+        $query = mysqli_query("SELECT * FROM membres WHERE utilisateur = '$dbUser' LIMIT 1");
+        $total = mysqli_num_rows($query);
+
+        if($total = "0"){
+            echo "L'utilisateur n'existe pas";
+            exit();
+        }
+        else{
+            $query = mysqli_query("SELECT * FROM membres WHERE utilisateur = '$dbUser' AND password = '$dbPwd' LIMIT 1");
+            $total = mysqli_num_rows($query);
+            if($total = "0"){
+                echo "Le mot de passe ne correspond pas";
+                exit();
+            }
+            else {
+                $_SESSION['utilisateur']= $dbUser;
+                header("location:".$_SERVER['HTTP_REFERER']);
+
+
+            }
+
+        }
+
+    }
+    else{header("location: index.php");
+    }
+
+    session_destroy();
+
+
+/*
     {
         $link = mysqli_connect($dbHost, $dbUser, $dbPwd, $dbName);
         if (!$link) {
@@ -36,5 +83,7 @@
 
 
     }
+
+*/
 
 ?>
