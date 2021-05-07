49af01f9c43afc6450d8a964a1602b9d3c1456e9
p1905532 <anne.merel@etu.univ-lyon1.fr>
2021-05-07 11:07:01 +0200

    supprimerPhoto adpatee pour admin/utilisateur

--- files/utilisateurFonctionalites/supprimerPhoto.php
+++ files/utilisateurFonctionalites/supprimerPhoto.php
@@ -31,11 +31,16 @@
             <form action='' method="POST">
                 <div>
                     <?php 
-           
+                        
                         if(!empty($_GET["id"])){
                             $utId = $_GET["id"]; // get the id of the user (lack of session variable )
-
-                            recuperePhotosUtilisateur($conn, $utId); // collects all the photo's of the $utId (certain user)
+                            if(checkIfUserIsAdmin($conn, $utId) === 'yes'){ // to make the difference between utilisateur and admin 
+                                $arr = utilisateur($conn); 
+                                foreach($arr as $id){
+                                    recuperePhotosUtilisateur($conn, $id);
+                                }
+                            }else recuperePhotosUtilisateur($conn, $utId); // collects all the photo's of the $utId (certain user)
+                            
                             formDelete($conn, $utId); // collects the photos that need to be deleted in an array and executes the sql command 
 
                         }else echo "login error";  // if there isn't a $utId defined, shows login error 
@@ -54,21 +59,23 @@
 
         if(isset($_POST['supprimer'])){
             if(!empty($_POST['photosSupprimer'])){
-                foreach($_POST['photosSupprimer'] as $nomFich){  // it's an array, the foreach allows us to tread element for element 
-                    supprimerPhoto($conn, $utId, $nomFich); // calls the fonction which deletes a photo which has the same $utId and $nomFich 
-                    header("Location: ./supprimerPhoto1.php?id=".$utId); // redirect to the "page d'accueil" so that the photo can be seen
+                foreach($_POST['photosSupprimer'] as $photoId){  // it's an array, the foreach allows us to tread element for element 
+                
+                    supprimerPhoto($conn, $photoId); // calls the fonction which deletes a photo which has the same $utId and $nomFich 
+                    header("Location: ./supprimerPhoto.php?id=".$utId); // redirect to the "page d'accueil" so that the photo can be seen
                 }
             }  
         }
     }
 
-    function supprimerPhoto($conn, $utId, $nomFich){ // fonction which deletes an row from a table whith utId = $utId , nomFich = $nomFich ;
-        $sql = "DELETE FROM `p1905532`.`Photo` WHERE utId ='".$utId."' AND nomFich ='".$nomFich."'" ; 
+    function supprimerPhoto($conn, $photoId){ // fonction which deletes an row from a table whith utId = $utId , nomFich = $nomFich ;
+        $sql = "DELETE FROM `p1905532`.`Photo` WHERE photoId ='".$photoId."'" ; 
         $result = $conn->query($sql);
     }
 
     function recuperePhotosUtilisateur($conn, $utId){ // fonction which collects all the photos from one user 
         $sql = "SELECT utId, nomFich, photoId FROM  `p1905532`.`Photo` WHERE utId ='".$utId."'"; 
+        //echo $sql; 
         $result = $conn->query($sql);
         if($result->num_rows > 0){
             $id = 0 ; 
@@ -77,7 +84,7 @@
                 $photoId = $row["photoId"];
                 $img = "<img src='../../images/" . $row["nomFich"] . "' class='singleImage'>"; // stores the html code for an image 
                 // concat the $img and the rest of the html code for an input type checkbox 
-                echo $img."<input type=\"checkbox\" id=\"".$row["nomFich"]."\" name='photosSupprimer[]' value=\"".$row["nomFich"]."\">"; 
+                echo $img."<input type=\"checkbox\" id=\"".$row["nomFich"]."\" name='photosSupprimer[]' value=\"".$row["photoId"]."\">"; // was $row["nomFich"]
                 $id++;  
             }
         }
