7433eea0c644f21f7dd844f593aca5e62849c199
p1905532 <anne.merel@etu.univ-lyon1.fr>
2021-05-07 09:42:54 +0200

    nettoyer le code cacher.php, fonction.php

--- files/utilisateurFonctionalites/ajoutePhoto.php
+++ files/utilisateurFonctionalites/ajoutePhoto.php
@@ -60,3 +60,87 @@
 </body>
 
 </html>
+
+<?php
+
+    function recupereNouvellePhoto($conn){ // fonction who gets all the information from the form 
+        $newNomFich = $newDescription = $newCateogrie = ''; // initialise before the if-statements --> we need them later in the fonction
+    
+            if(!empty($_POST['inputCategorie'])) {
+                if($_POST['inputCategorie'] === 'none'){ // if there isn't a category selected 
+                    echo 'Vous n\'avez pas selectionée une categorie .<br>';  
+                }else $newCateogrie = $_POST['inputCategorie']; 
+            } else {
+                echo 'Vous n\'avez pas selectionée une categorie .<br>';
+            }
+            
+            if(!empty($_FILES['file'])) {
+                $newNomFich = $_FILES['file']['name']; // get the name of the new file 
+                
+            } else {
+                echo 'Vous n\'avez pas choisi une image. <br>'; // when the input file is empty
+            }
+            
+            if(!empty($_POST['inputDescription'])) {
+                if($_POST['inputDescription'] === ''){
+                    echo 'Vous n\'avez pas donne une description.<br>';
+                }else $newDescription = $_POST['inputDescription'];
+            } else {
+                echo 'Vous n\'avez pas donne une description.<br>';  
+            }
+
+        $newPhotoId = random_int(500, 950); // generates a random number as photoId  
+        
+        insertPhoto($conn, $newPhotoId, $newNomFich, $newDescription, $newCateogrie); // inserts the photo into the database 
+        
+    }
+
+
+    function insertPhoto($conn, $photoId, $nomFich, $description, $catId){ // adds a photo to the database 
+        $utId = 'AnneVogel'; // needs to be session variable 
+        $sqlValues = "('".$photoId."', '".$nomFich."','".$description."','".$catId."','".$utId."')";  
+        $sql = "INSERT INTO `p1905532`.`Photo` (photoId, nomFich, description, catId, utId) VALUES".$sqlValues; 
+             
+        if($conn -> query($sql) === TRUE){
+            echo "Nouvelle photo ajoute dans le database"; 
+            header("Location: ../index.php?ajout=succes");
+
+        } 
+    }
+
+    
+    //brief function which uploads the photo to our /image repository 
+    function ajoutePhoto($conn){
+        /// upload image 
+        if(isset($_POST['submit'])){
+        
+            // upload file (== image)
+            $file = $_FILES['file']; // is an array [name, type, tmp_name, error, size] 
+            
+            $fileName = $_FILES['file']['name'];
+            $fileTmpName = $_FILES['file']['tmp_name']; // temporary location of file 
+            $fileSize = $_FILES['file']['size']; // size of file 
+            $fileError = $_FILES['file']['error']; // error  
+            $fileType = $_FILES['file']['type']; // type of file 
+            
+            $fileExt = explode('.', $fileName); // file extension, explode takes the part after the . <-- extension
+            $fileActualExt = strtolower(end($fileExt));// extension into lowercase, end() --> last element of array
+            $fileFirstName = $fileExt[0]; 
+        
+            $allowed = array('gif', 'jpeg', 'png'); // extension allowed 
+        
+            if(in_array($fileActualExt, $allowed)){ // extension is allowed
+                if($fileError === 0 ){ // if 0 --> no errors 
+                    if($fileSize <  100000){ // taille of image max = 100 ko = 100 * 8 o = 800000      
+                        $fileDestination = '../images/'.$fileFirstName.".".$fileActualExt; // place where it's going to be
+              
+                        move_uploaded_file($fileTmpName, $fileDestination);
+                        recupereNouvellePhoto($conn); 
+                        
+                    }else echo "your image is too big"; 
+                } else echo "There was an error uploading your file";  
+            }else echo "You cannot upload files of this type <br> You either didn't select a file or the file is the wrong type <br> The supported type is jpeg, png, gif";
+        }
+    }
+
+?>
--- files/utilisateurFonctionalites/cacher.php
+++ files/utilisateurFonctionalites/cacher.php
@@ -1,18 +1,18 @@
 <?php 
 
-
+// generates the html code for the photo's whiche aren't hidden
 function formCacher($conn, $utId){
     if(!empty($_GET["id"])){
-        $utId = $_GET["id"];
+        $utId = $_GET["id"]; // needs to be session variable 
 
-        recuperePhotosUtilisateur($conn, $utId, 0);
+        recuperePhotosUtilisateur($conn, $utId, 0); // html code 
        
         if(isset($_POST['cacher'])){
            if(!empty($_POST['photos'])){
-               foreach($_POST['photos'] as $photoId){
+               foreach($_POST['photos'] as $photoId){ // form with checkboxes, return as an array
                    
-                   remplaceCacherDansPhoto($conn, $utId, $photoId , 1);
-                   header("Location: ./cacherPhoto.php?id=".$utId);
+                   remplaceCacherDansPhoto($conn, $utId, $photoId , 1); // we remplace the "cacher" dans le table avec 1
+                   header("Location: ./cacherPhoto.php?id=".$utId); // we refresh the page so that the changes are directly visible
                }
            }
        
@@ -20,6 +20,7 @@ function formCacher($conn, $utId){
     }
 }
 
+// almost the same fonction as the one above. Only now, it displays the photos which are hidden
 function formAfficher($conn, $utId){
     if(!empty($_GET["id"])){
         $utId = $_GET["id"];
@@ -30,7 +31,7 @@ function formAfficher($conn, $utId){
            if(!empty($_POST['photos'])){
                foreach($_POST['photos'] as $photoId){
                    
-                   remplaceCacherDansPhoto($conn, $utId, $photoId , 0);
+                   remplaceCacherDansPhoto($conn, $utId, $photoId , 0); // and remplaces the 1 in "cacher" to 0 
                    header("Location: ./cacherPhoto.php?id=".$utId);
                }
            }
@@ -39,38 +40,38 @@ function formAfficher($conn, $utId){
     }
 }
 
+// function which generates the html code to display the image with checkboxes (<-- needs to be inside a <form>)
 function recuperePhotosUtilisateur($conn, $utId,  $cacher){
     $sql = "SELECT utId, nomFich, photoId FROM  `p1905532`.`Photo` WHERE utId ='".$utId."' AND cacher ='".$cacher."'"; 
     $result = $conn->query($sql);
     if($result->num_rows > 0){
-        $id = 0 ; 
         while($row = $result -> fetch_assoc()){
             
             $photoId = $row["photoId"];
             $img = "<img src='../../images/" . $row["nomFich"] . "' class='singleImage'>"; 
             
-            echo $img."<input type=\"checkbox\" id=\"".$row["nomFich"]."\" name='photos[]' value=\"".$row["photoId"]."\">"; 
-            $id++;  
+            echo $img."<input type=\"checkbox\" id=\"".$row["nomFich"]."\" name='photos[]' value=\"".$row["photoId"]."\">";  
         }
     }
 }
 
+// fonction which executes the REPLACE INTO SET command sql 
 function remplaceCacherDansPhoto($conn, $utId, $photoId , $cacher){
-    $sql = "SELECT * FROM `p1905532`.`Photo` WHERE photoId =".$photoId;
+    $sql = "SELECT * FROM `p1905532`.`Photo` WHERE photoId =".$photoId; // gets alle the current information of a photo 
     $result = $conn->query($sql);
     if($result->num_rows > 0){
         while($row = $result -> fetch_assoc()){
 
         $sql1 = "REPLACE INTO `p1905532`.`Photo` SET "; 
-            $l1 = "photoId ='".$photoId."',";
-            $l2 = "nomFich ='".$row["nomFich"]."',";
-            $l3 = "description ='".$row["description"]."',";
-            $l4 = "catId =".$row["catId"].",";
-            $l5 = "utId ='".$row["utId"]."',"; 
-            $l6 = "cacher = ".$cacher; 
-        $sql1 = $sql1.$l1.$l2.$l3.$l4.$l5.$l6; 
-     //   echo $sql1 ; 
-        $result2 = $conn->query($sql1);
+        $l1 = "photoId ='".$photoId."',";
+        $l2 = "nomFich ='".$row["nomFich"]."',";
+        $l3 = "description ='".$row["description"]."',";
+        $l4 = "catId =".$row["catId"].",";
+        $l5 = "utId ='".$row["utId"]."',"; 
+        $l6 = "cacher = ".$cacher; 
+        $sql1 = $sql1.$l1.$l2.$l3.$l4.$l5.$l6; // concat all the parts together into one string 
+    
+        $result2 = $conn->query($sql1); // execute the command 
         }
     }
 }
